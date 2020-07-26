6968de0e6be41285afce999cbae961839dd70215
reddymeghana <65498754+reddymeghana@users.noreply.github.com>
2020-07-26 21:42:19 +0530

    Update detailsstudent.sol

--- detailsstudent.sol
+++ detailsstudent.sol
@@ -19,6 +19,13 @@ function Students(string newName,string newRollno,string newBatch,string newStat
     m3=newM3;
     m4=newM4;
 }
+function setNewDetails(string newStatus,uint newM1,uint newM2,uint newM3,uint newM4)public{
+     status=newStatus;
+    m1=newM1;
+    m2=newM2;
+    m3=newM3;
+    m4=newM4;
+}
 function StudentDetails()public view returns(string,string,string,string,uint,uint,uint,uint)
 {
     return(name,rollno,batch,status,m1,m2,m3,m4);
