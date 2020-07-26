9a3eecb3dbb7ae3037f61513ea04e350649ed5e8
reddymeghana <65498754+reddymeghana@users.noreply.github.com>
2020-07-26 21:23:32 +0530

    Create detailsstudent.sol

---
+++ detailsstudent.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.21 < 0.6.12;
+contract Students{
+    string public name;
+    string public rollno;
+    string public batch;
+    string public status;
+    uint public m1;
+    uint public m2;
+    uint public m3;
+    uint public m4;
+
+function Students(string newName,string newRollno,string newBatch,string newStatus,uint newM1,uint newM2,uint newM3,uint newM4){
+    name=newName;
+    rollno=newRollno;
+    batch=newBatch;
+    status=newStatus;
+    m1=newM1;
+    m2=newM2;
+    m3=newM3;
+    m4=newM4;
+}
+function StudentDetails()public view returns(string,string,string,string,uint,uint,uint,uint)
+{
+    return(name,rollno,batch,status,m1,m2,m3,m4);
+}
+}
